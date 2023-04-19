773c5b1178b59a92add5ec4c3caa8115d1bb8999
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-04-18 22:00:16 -0300

    style: estilo do slider

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -1,61 +1,71 @@
 import React from 'react'
 import styled from 'styled-components';
+import Image from 'next/image';
+
+// assets
+import slider1 from "../../../public/slider/img-carrousel.svg";
+import slider2 from "../../../public/slider/img-carrousel1.svg";
+import slider3 from "../../../public/slider/img-carrousel2.svg";
+import slider4 from "../../../public/slider/img-carrousel3.svg";
+import slider5 from "../../../public/slider/img-carrousel4.svg";
+import slider6 from "../../../public/slider/img-carrousel5.svg";
 
 // swiper
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, EffectCoverflow, A11y } from 'swiper';
- 
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
 // swiper css
 import 'swiper/swiper.min.css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
 import './Slider.css';
 
 
 const ImageContainer = styled.div`
     width: 100%;
-    height: 100px;
+		height: 20rem;
 
-    border: 1px solid #000;
+    > img {
+        width: 100%;
+				height: 100%;
+        object-fit: cover;
+    }
 
 `
 
 const Slider = () => {
-    const settings: SwiperProps = {
-        modules: [Navigation, Pagination, A11y, EffectCoverflow],
-        effect: 'coverflow',
-        slidesPerView: 3,
-        spaceBetween: 20,
-        navigation: true,
-        pagination: {
-            clickable: true,
-            type: 'bullets',
-        },
-        loop: true // verificar
-    }
+	const imagesSlider = [slider1, slider2, slider3, slider4, slider5, slider6]
+	const settings: SwiperProps = {
+		modules: [Navigation, Pagination, A11y, Autoplay],
+		slidesPerView: 4,
+		spaceBetween: 30,
+		navigation: true,
+		pagination: {
+			clickable: true,
+			type: 'bullets',
+		},
+		loop: true,
+		centeredSlides: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+	}
 
-    return (
-        <Swiper
-            {...settings}
-        >
-            <SwiperSlide>
-                <ImageContainer>
-                </ImageContainer>
-            </SwiperSlide>
-            <SwiperSlide>
-                <ImageContainer>
-                </ImageContainer>
-            </SwiperSlide>
-            <SwiperSlide>
-                <ImageContainer>
-                </ImageContainer>
-            </SwiperSlide>
-            <SwiperSlide>
-                <ImageContainer>
-                </ImageContainer>
-            </SwiperSlide>
-        </Swiper>
-    )
+	return (
+		<Swiper
+			{...settings}
+		>
+			{imagesSlider.map(image => (
+				<SwiperSlide>
+					<ImageContainer>
+						<Image src={image} alt='Imagem slider' />
+					</ImageContainer>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	)
 }
 
 export default Slider;
