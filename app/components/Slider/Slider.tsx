7f295d78a1c13cb0c46135f9b1eacd37368f25eb
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-04-19 01:19:55 -0300

    refactor: modifica as imagens do slider

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -9,6 +9,10 @@ import slider3 from "../../../public/slider/img-carrousel2.svg";
 import slider4 from "../../../public/slider/img-carrousel3.svg";
 import slider5 from "../../../public/slider/img-carrousel4.svg";
 import slider6 from "../../../public/slider/img-carrousel5.svg";
+import slider7 from "../../../public/slider/img-carrousel6.svg";
+import slider8 from "../../../public/slider/img-carrousel7.svg";
+import slider9 from "../../../public/slider/img-carrousel8.svg";
+import slider10 from "../../../public/slider/img-carrousel9.svg";
 
 // swiper
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
@@ -24,48 +28,48 @@ import './Slider.css';
 
 const ImageContainer = styled.div`
     width: 100%;
-		height: 20rem;
+		height: 21rem;
 
     > img {
         width: 100%;
-				height: 100%;
+		height: 100%;
         object-fit: cover;
     }
 
 `
 
 const Slider = () => {
-	const imagesSlider = [slider1, slider2, slider3, slider4, slider5, slider6]
-	const settings: SwiperProps = {
-		modules: [Navigation, Pagination, A11y, Autoplay],
-		slidesPerView: 4,
-		spaceBetween: 30,
-		navigation: true,
-		pagination: {
-			clickable: true,
-			type: 'bullets',
-		},
-		loop: true,
-		centeredSlides: true,
-		autoplay: {
-			delay: 2500,
-			disableOnInteraction: false,
-		},
-	}
+  const imagesSlider = [slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider8, slider9, slider10];
+  const settings: SwiperProps = {
+    modules: [Navigation, Pagination, A11y, Autoplay],
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: true,
+    pagination: {
+      clickable: true,
+      type: 'bullets',
+    },
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  }
 
-	return (
-		<Swiper
-			{...settings}
-		>
-			{imagesSlider.map(image => (
-				<SwiperSlide>
-					<ImageContainer>
-						<Image src={image} alt='Imagem slider' />
-					</ImageContainer>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	)
+  return (
+    <Swiper
+      {...settings}
+    >
+      {imagesSlider.map(image => (
+        <SwiperSlide>
+          <ImageContainer>
+            <Image src={image} alt='Imagem slider' />
+          </ImageContainer>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
 }
 
 export default Slider;
